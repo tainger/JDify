103c0a0e4f3b3536093aa77719b10f74e9ea8cfc
jingdi <jingdi@jingdideMacBook-Pro.local>
2019-03-07 16:46:41 +0800

    test add and remove route dynamic

--- example/src/main/java/io/terminus/dalaran/example/Test.java
+++ example/src/main/java/io/terminus/dalaran/example/Test.java
@@ -2,6 +2,7 @@ package io.terminus.dalaran.example;
 
 import com.google.gson.Gson;
 import io.terminus.dalaran.*;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.ModelCamelContext;
 
@@ -13,23 +14,34 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         DalaranComponentLoader.loadComponents();
+        ModelCamelContext camelContext = new DefaultCamelContext();
+        camelContext.start();
+        RouteBuilder route1 = buildRoute("/example-flow.json");
+        camelContext.addRoutes(route1);
 
+        Thread.sleep(60000);
+
+        RouteBuilder route2 = buildRoute("/example-flow-other.json");
+        camelContext.addRoutes(route2);
+
+        Thread.sleep(60000);
+        
+        camelContext.stopRoute(route1.getRouteCollection().getRoutes().get(0).getId());
+
+        synchronized (Test.class) {
+            Test.class.wait();
+        }
+    }
+
+    private static RouteBuilder buildRoute(String json) {
         Gson gson = new Gson();
         Gson newGson = gson.newBuilder().
                 registerTypeAdapter(MessageFlow.Listener.class, new DalaranComponentTypeAdapter(gson, "listener")).
                 registerTypeAdapter(MessageFlow.Endpoint.class, new DalaranComponentTypeAdapter(gson, "endpoint")).create();
-        InputStream in = Test.class.getResourceAsStream("/example-flow.json");
+        InputStream in = Test.class.getResourceAsStream(json);
 
         Reader reader = new InputStreamReader(in);
         MessageFlow messageFlow = newGson.fromJson(reader, MessageFlow.class);
-        CamelRouteBuilder routeBuilder = new CamelRouteBuilder(messageFlow);
-        ModelCamelContext camelContext = new DefaultCamelContext();
-        camelContext.start();
-        camelContext.addRoutes(routeBuilder);
-        synchronized (Test.class) {
-            Test.class.wait();
-        }
-
+        return new CamelRouteBuilder(messageFlow);
     }
-
 }
