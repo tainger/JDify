be331742e1630a0769f09e32c2c9b516936e24c1
jingdi <jingdi@jingdideMacBook-Pro.local>
2019-03-21 13:30:25 +0800

    remove useless code

--- components/message-translator/src/main/kotlin/io/terminus/dalaran/component/message/convert/custom/CustomMapperProcessor.java
+++ components/message-translator/src/main/kotlin/io/terminus/dalaran/component/message/convert/custom/CustomMapperProcessor.java
@@ -21,41 +21,9 @@ public class CustomMapperProcessor implements Processor {
         Map<String, String> messageMapping = gson.fromJson(gson.toJson(exchange.getIn().getHeader("MessageMapping")), Map.class);
         List<String> target = gson.fromJson(gson.toJson(exchange.getIn().getHeader("target")), List.class);
         List<String> destination = gson.fromJson(gson.toJson(exchange.getIn().getHeader("destination")), List.class);
-
-//        Map<String, Object> targetMessage = exchange.getIn().getBody(HashedMap.class);
-
         Map<String, Object> targetBody = gson.fromJson((String) exchange.getIn().getBody(), Map.class);
-
-//        Map<String, Object> destinationBody = buildDestinationBody(messageMapping, target, destination, targetBody);
         Map<String, Object> destinationBody = convertWithJXPath(messageMapping, destination, targetBody);
         exchange.getOut().setBody(destinationBody);
-    }
-
-//    private Map<String, Object> buildDestinationBody(List<SingleFieldMapping> mappings, Map<String, Object> targetBody, DalaranMessage targetMsg, DalaranMessage destinationMsg) {
-//        Map<String, Object> destinationBody = new HashMap<>();
-//        for (String destinationField : destinationMsg.getFields().keySet()) {
-//            for (SingleFieldMapping mapping : mappings) {
-//                Map<String, String> fieldsMapping = mapping.getMapping();
-//                if (fieldsMapping.containsKey(destinationField)) {
-//                    String targetField = fieldsMapping.get(destinationField);
-//                    Object fieldVal = targetBody.get(targetField);
-//                    destinationBody.put(destinationField, fieldVal);
-//                }
-//            }
-//        }
-//        return destinationBody;
-//    }
-
-    private Map<String, Object> buildDestinationBody(Map<String, String> messageMapping, List<String> target, List<String> destination, Map<String, Object> targetBody) {
-        Map<String, Object> destinationBody = new HashMap<>();
-        for (String destinationField : destination) {
-            if (messageMapping.containsKey(destinationField)) {
-                String targetField = messageMapping.get(destinationField);
-                Object fieldVal = targetBody.get(targetField);
-                destinationBody.put(destinationField, fieldVal);
-            }
-        }
-        return destinationBody;
     }
 
     private Map<String, Object> convertWithJXPath(Map<String, String> messageMapping, List<String> destination, Map<String, Object> targetBody) {
@@ -72,8 +40,4 @@ public class CustomMapperProcessor implements Processor {
 
         return ((Map<String, Object>)destinationContext.getContextBean());
     }
-
-//    private Map<String, Object> convertTargetBody(Message message) {
-//
-//    }
 }
