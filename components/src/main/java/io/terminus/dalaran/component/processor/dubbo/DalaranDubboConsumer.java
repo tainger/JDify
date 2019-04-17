4a43bcb91fba305e7c72f505e55622e9aca70ff1
耿荣 <gr@terminus.io>
2019-04-17 13:45:43 +0800

    add dubbo consumer component

---
+++ components/src/main/java/io/terminus/dalaran/component/processor/dubbo/DalaranDubboConsumer.java
@@ -0,0 +1,19 @@
+package io.terminus.dalaran.component.processor.dubbo;
+
+import io.terminus.dalaran.BodyMode;
+import io.terminus.dalaran.DalaranProcessor;
+import io.terminus.dalaran.annotation.Processor;
+import org.apache.camel.model.ProcessorDefinition;
+
+@Processor(value = "dubbo-consumer", bodyMode = BodyMode.Object, configType = DalaranDubboConsumerConfig.class)
+public class DalaranDubboConsumer implements DalaranProcessor<DalaranDubboConsumerConfig> {
+
+    private static final String DUBBO_PROVIDER_URI = "dubbo:?registryAddress=%s&serviceId=%s&method=%s&version=%s";
+
+    @Override
+    public void configure(ProcessorDefinition route, DalaranDubboConsumerConfig config) {
+        String uri = String.format(DUBBO_PROVIDER_URI, config.getRegistryAddress(),
+                config.getServiceId(), config.getMethod(), config.getVersion());
+        route.to(uri);
+    }
+}
