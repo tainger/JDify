f4ea8abfaa4e37936fc45ec4ab9d80aedf5639c2
jingdi <jingdi@jingdideMacBook-Pro.local>
2019-06-27 15:40:00 +0800

    save code

--- components/src/test/java/io/terminus/dalaran/component/processor/RouterTest.java
+++ components/src/test/java/io/terminus/dalaran/component/processor/RouterTest.java
@@ -2,6 +2,9 @@ package io.terminus.dalaran.component.processor;
 
 import io.terminus.dalaran.component.BasicProcessorTest;
 import io.terminus.dalaran.component.processor.route.DalaranRouter;
+import org.apache.camel.ProducerTemplate;
+import org.apache.commons.collections.MapUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -12,14 +15,28 @@ import java.util.Map;
  */
 public class RouterTest extends BasicProcessorTest {
 
+    private static final int id = 3;
+
+    /**
+     * id = 1: insert branch01
+     * id = 2: insert branch02
+     * id = 3: insert branch03
+     * otherwise: insert default
+     */
     @Test
     public void testRouterTest() {
-
-
-
         DalaranRouter router = new DalaranRouter();
         Map<String, String> config = new HashMap<>();
 
+        ProducerTemplate template = getProcessorTemplate(router, config);
+        Assert.assertNotNull(template);
 
+        Map<String, Map<String, Object>> requestBody = new HashMap<>();
+        Map<String, Object> body = new HashMap<>();
+        body.put("id", id);
+        requestBody.put("user", body);
+
+        Object result = template.requestBody(requestBody);
+        Assert.assertNotNull(result);
     }
 }
