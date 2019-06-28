90b0ca809392a0212abcdcbad90092df80aca135
jingdi <jingdi@jingdideMacBook-Pro.local>
2019-06-28 11:06:56 +0800

    add rocketmq config

--- components/src/main/java/io/terminus/dalaran/component/trigger/rocketmq/RocketMQConsumerConfig.java
+++ components/src/main/java/io/terminus/dalaran/component/trigger/rocketmq/RocketMQConsumerConfig.java
@@ -1,5 +1,7 @@
 package io.terminus.dalaran.component.trigger.rocketmq;
 
+import io.terminus.dalaran.core.component.FieldInputType;
+import io.terminus.dalaran.core.component.annotation.ConfigFieldInfo;
 import lombok.Data;
 
 /**
@@ -8,9 +10,12 @@ import lombok.Data;
 @Data
 public class RocketMQConsumerConfig {
 
+    @ConfigFieldInfo(label = "注册中心", inputType = FieldInputType.String)
     private String nameServer;
 
+    @ConfigFieldInfo(label = "主题", inputType = FieldInputType.String)
     private String topic;
 
+    @ConfigFieldInfo(label = "消费者组", inputType = FieldInputType.String)
     private String consumerGroup;
 }
