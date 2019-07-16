3a2837d10c361d46f4e3a1516551264c0177ad52
耿荣 <gr@terminus.io>
2019-07-16 15:39:19 +0800

    update axure

--- core/src/main/java/io/terminus/dalaran/core/model/FieldType.java
+++ core/src/main/java/io/terminus/dalaran/core/model/FieldType.java
@@ -4,5 +4,19 @@ package io.terminus.dalaran.core.model;
  * Created by jingdi on 2019/3/25
  */
 public enum FieldType {
-    STRING, INTEGER, LONG, SHORT, FLOAT, DOUBLE, DATE, BOOLEAN, NUMBER, ARRAY, MAP, OBJECT
+    STRING, INTEGER, LONG, SHORT, FLOAT, DOUBLE, DATE, BOOLEAN, NUMBER, ARRAY(false), MAP(false), OBJECT(false);
+
+    private boolean basicType;
+
+    FieldType() {
+        this.basicType = true;
+    }
+
+    FieldType(boolean basicType) {
+        this.basicType = basicType;
+    }
+
+    public boolean isBasicType() {
+        return basicType;
+    }
 }
