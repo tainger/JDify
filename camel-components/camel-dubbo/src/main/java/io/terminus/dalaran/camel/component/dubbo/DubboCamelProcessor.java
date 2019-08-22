72351d0fbd83a2e02114098c10088ff9eb442fdd
耿荣 <gr@terminus.io>
2019-08-22 14:00:11 +0800

    update axure & fix dubbo component create

--- camel-components/camel-dubbo/src/main/java/io/terminus/dalaran/camel/component/dubbo/DubboCamelProcessor.java
+++ camel-components/camel-dubbo/src/main/java/io/terminus/dalaran/camel/component/dubbo/DubboCamelProcessor.java
@@ -7,14 +7,13 @@ import org.apache.camel.impl.DefaultProducer;
 
 public class DubboCamelProcessor extends DefaultProducer {
 
-    private final GenericService genericService;
-
+    private final DubboEndpoint endpoint;
     private final String method;
     private final String parameterType;
 
     public DubboCamelProcessor(DubboEndpoint endpoint) {
         super(endpoint);
-        this.genericService = endpoint.getGenericService();
+        this.endpoint = endpoint;
         this.method = endpoint.getMethod();
         this.parameterType = endpoint.getParameterType();
     }
@@ -24,9 +23,9 @@ public class DubboCamelProcessor extends DefaultProducer {
         Object arg = exchange.getIn().getBody();
         Object result;
         if (StringUtils.isNotEmpty(parameterType)) {
-            result = genericService.$invoke(method, new String[]{parameterType}, new Object[]{arg});
+            result = endpoint.getGenericService().$invoke(method, new String[]{parameterType}, new Object[]{arg});
         } else {
-            result = genericService.$invoke(method, new String[]{}, new Object[]{});
+            result = endpoint.getGenericService().$invoke(method, new String[]{}, new Object[]{});
         }
         exchange.getOut().setBody(result);
     }
