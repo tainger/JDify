d6f17b6026a884202eb489bf4d6682d8c4945fdd
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-06-28 15:02:20 +0800

    save

---
+++ components/src/main/java/io/terminus/dalaran/component/utils/AESUtils.java
@@ -0,0 +1,15 @@
+package io.terminus.dalaran.component.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+@Slf4j
+public class AESUtils {
+
+    public static String AESEncrypt(String origin) {
+        if (StringUtils.isBlank(origin)) {
+            return null;
+        }
+        return null;
+    }
+}
