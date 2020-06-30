3bb21bf60a670f199f5e1fcafc7e5d51b5aec023
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-06-30 17:17:50 +0800

    fix json to xml

--- components/src/main/java/io/terminus/dalaran/function/xml/ToXmlFunction.java
+++ components/src/main/java/io/terminus/dalaran/function/xml/ToXmlFunction.java
@@ -3,6 +3,10 @@ package io.terminus.dalaran.function.xml;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.terminus.dalaran.core.component.annotation.MappingFunction;
+import io.terminus.dalaran.function.model.FunctionConstants;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 
 import java.io.IOException;
 
@@ -11,7 +15,15 @@ public class ToXmlFunction {
 
     private final ObjectMapper xmlMapper = new XmlMapper();
 
-    public Object execute(Object data) throws IOException {
+    public Object execute(Object data, String type) throws IOException {
+        switch (type) {
+            case FunctionConstants.ARRAY:
+                JSONArray jsonArray = new JSONArray(data);
+                return XML.toString(jsonArray);
+            case FunctionConstants.OBJECT:
+                JSONObject jsonObject = new JSONObject(data);
+                return XML.toString(jsonObject);
+        }
         return xmlMapper.writeValueAsString(data);
     }
 }
