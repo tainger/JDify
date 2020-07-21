accc266ed673a5243d230860da0e8f13ab4e36fd
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-07-21 19:30:18 +0800

    fix custom upload

--- components/src/main/java/io/terminus/dalaran/component/processor/ftp/custom/CustomFTPUpLoadPreProcessor.java
+++ components/src/main/java/io/terminus/dalaran/component/processor/ftp/custom/CustomFTPUpLoadPreProcessor.java
@@ -1,9 +1,11 @@
 package io.terminus.dalaran.component.processor.ftp.custom;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONPath;
 import freemarker.template.Template;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.StringWriter;
@@ -23,13 +25,30 @@ public class CustomFTPUpLoadPreProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        String fileName = config.getFileRoot();
-        if (StringUtils.isNotBlank(config.getDatePattern())) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat(config.getDatePattern());
-            fileName += "." +  dateFormat.format(new Date());
-        }
-        if (StringUtils.isNotBlank(config.getFileSuffix())) {
-            fileName += "." + config.getFileSuffix();
+        String fileName;
+        if (config.isDynamicFileName()) {
+            Object in = exchange.getIn().getBody();
+            JSON body;
+            if (in instanceof String) {
+                body = JSON.parseObject((String)in, JSON.class);
+            } else if (in instanceof byte[]) {
+                body = JSON.parseObject(IOUtils.toString((byte[])in), JSON.class);
+            } else {
+                body = JSON.parseObject(JSON.toJSONString(in), JSON.class);
+            }
+            if (!JSONPath.contains(body, config.getDynamicPath())) {
+                throw new RuntimeException("body: " + body + ", no file name");
+            }
+            fileName = JSONPath.eval(body, config.getDynamicPath()).toString();
+        } else {
+            fileName = config.getFileRoot();
+            if (StringUtils.isNotBlank(config.getDatePattern())) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat(config.getDatePattern());
+                fileName += "." +  dateFormat.format(new Date());
+            }
+            if (StringUtils.isNotBlank(config.getFileSuffix())) {
+                fileName += "." + config.getFileSuffix();
+            }
         }
         exchange.getOut().setHeaders(exchange.getIn().getHeaders());
         exchange.getOut().setHeader("CamelOverruleFileName", fileName);
