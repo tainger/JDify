3c6257250b081791b52bd59dd7db95a789eb5cf5
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-07-29 14:22:01 +0800

    add oracle

--- components/src/test/java/io/terminus/dalaran/component/utils/SignUtilsTest.java
+++ components/src/test/java/io/terminus/dalaran/component/utils/SignUtilsTest.java
@@ -227,7 +227,7 @@ public class SignUtilsTest {
 
         String s = AESUtils.encrypt("001", "89622015104709087435617163207900");
 
-        String ss = AESUtils.decrypt(d, "8962201510470905");
+        String ss = AESUtils.decrypt("9lSbfM7InsPS+m34xTR8kA==", "8962201510470905");
 
 
         Assert.assertTrue(false);
