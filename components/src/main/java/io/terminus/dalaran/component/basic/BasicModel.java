fb68a6d137d5a00c9019ce4de463bfd1e36c952f
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-09-29 16:06:47 +0800

    update

--- components/src/main/java/io/terminus/dalaran/component/basic/BasicModel.java
+++ components/src/main/java/io/terminus/dalaran/component/basic/BasicModel.java
@@ -2,17 +2,17 @@ package io.terminus.dalaran.component.basic;
 
 import io.terminus.dalaran.FieldInputType;
 import io.terminus.dalaran.core.component.DalaranBasicComponent;
+import io.terminus.dalaran.core.component.annotation.ConfigFieldInfo;
 import io.terminus.dalaran.core.component.annotation.DynamicModel;
-import io.terminus.dalaran.model.annotation.ModelFieldInfo;
 import lombok.Data;
 
 @Data
 @DynamicModel(value = "Model")
 public class BasicModel implements DalaranBasicComponent {
 
-    @ModelFieldInfo(label = "名称", inputType = FieldInputType.String)
+    @ConfigFieldInfo(label = "名称", inputType = FieldInputType.String)
     private String name;
 
-    @ModelFieldInfo(label = "类型", inputType = FieldInputType.ModelSelector)
+    @ConfigFieldInfo(label = "类型", inputType = FieldInputType.ModelSelector)
     private String modelType;
 }
