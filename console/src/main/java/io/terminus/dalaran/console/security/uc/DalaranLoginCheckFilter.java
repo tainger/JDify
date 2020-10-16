e077522912aca7e8f8ac7027688c5b0611a863fe
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-10-16 13:43:41 +0800

    fix swagger filter

--- console/src/main/java/io/terminus/dalaran/console/security/uc/DalaranLoginCheckFilter.java
+++ console/src/main/java/io/terminus/dalaran/console/security/uc/DalaranLoginCheckFilter.java
@@ -4,21 +4,30 @@ import com.alibaba.fastjson.JSON;
 import io.terminus.dalaran.model.security.CustomResponseBody;
 import io.terminus.dalaran.model.security.SecurityType;
 import io.terminus.draco.web.autoconfig.context.UserContext;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
-@WebFilter(filterName = "DalaranLoginCheckFilter", urlPatterns = {"/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security",
-        "/swagger-ui.html", "/webjars/**", "/actuator/**"})
+@WebFilter(filterName = "DalaranLoginCheckFilter", urlPatterns = {"/*"})
 public class DalaranLoginCheckFilter implements Filter {
 
     private static final int ERROR_CODE  = 403;
+
+    private static final Set<String> ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(
+            Arrays.asList("/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security",
+                    "/swagger-ui.html", "/webjars", "/actuator")));
 
     @Value(value = "${draco.filter}")
     private boolean filter;
@@ -28,7 +37,10 @@ public class DalaranLoginCheckFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if (filter && UserContext.getUserInfo() == null) {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");
+
+        if (filter && UserContext.getUserInfo() == null && !containsPath(ALLOWED_PATHS, path)) {
             CustomResponseBody responseBody = new CustomResponseBody();
             responseBody.setType(SecurityType.UC);
             responseBody.setMessage(loginUrl);
@@ -52,5 +64,14 @@ public class DalaranLoginCheckFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+    private boolean containsPath(Set<String> paths, String request) {
+        for (String path : paths) {
+            if (StringUtils.startsWithIgnoreCase(request, path)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
