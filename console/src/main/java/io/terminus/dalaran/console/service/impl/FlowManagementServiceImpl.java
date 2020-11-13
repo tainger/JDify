1983805897a01b5d06ec406d99a46ffd31ad8977
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-11-13 10:26:02 +0800

    check trigger model

--- console/src/main/java/io/terminus/dalaran/console/service/impl/FlowManagementServiceImpl.java
+++ console/src/main/java/io/terminus/dalaran/console/service/impl/FlowManagementServiceImpl.java
@@ -384,8 +384,12 @@ public class FlowManagementServiceImpl implements FlowManagementService {
         flowEntity.setTriggerType(triggerFlow.getTriggerType());
         flowEntity.setTriggerConfig(JSON.toJSONString(triggerFlow.getTriggerConfig()));
         flowEntity.setModuleId(triggerFlow.getModuleId());
-        flowEntity.setInModel(Long.valueOf(JSON.toJSONString(triggerFlow.getTriggerConfig().get("inModelId"))));
-        flowEntity.setOutModel(Long.valueOf(JSON.toJSONString(triggerFlow.getTriggerConfig().get("outModelId"))));
+        if (triggerFlow.getTriggerConfig().get("inModelId") != null) {
+            flowEntity.setInModel(Long.valueOf(JSON.toJSONString(triggerFlow.getTriggerConfig().get("inModelId"))));
+        }
+        if (triggerFlow.getTriggerConfig().get("outModelId") != null) {
+            flowEntity.setOutModel(Long.valueOf(JSON.toJSONString(triggerFlow.getTriggerConfig().get("outModelId"))));
+        }
         flowEntity.setPipeline(pipeline);
         flowEntity.setTracing(triggerFlow.isTracing());
         flowEntity.setDescription(triggerFlow.getDescription());
