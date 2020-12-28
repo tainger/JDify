d055f832caa6e1eb0f7aa858e04e74210f61f7ce
jingdi <jingdi@jingdideMacBook-Pro.local>
2020-12-28 15:00:45 +0800

    update json parse

--- dalaran-component/dalaran-common/src/main/java/io/terminus/dalaran/model/json/JsonMarshalPreProcessor.java
+++ dalaran-component/dalaran-common/src/main/java/io/terminus/dalaran/model/json/JsonMarshalPreProcessor.java
@@ -1,6 +1,7 @@
 package io.terminus.dalaran.model.json;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -10,7 +11,10 @@ public class JsonMarshalPreProcessor implements Processor {
     public void process(Exchange exchange) throws Exception {
         Object in = exchange.getIn().getBody();
         if (in instanceof byte[]) {
-            exchange.getOut().setBody(JSON.parse((byte[])in));
+            exchange.getOut().setBody(JSON.toJSONString(JSON.parse((byte[])in), SerializerFeature.WriteMapNullValue));
+            exchange.getOut().setHeaders(exchange.getIn().getHeaders());
+        } else {
+            exchange.getOut().setBody(JSON.toJSONString(in, SerializerFeature.WriteMapNullValue));
             exchange.getOut().setHeaders(exchange.getIn().getHeaders());
         }
     }
