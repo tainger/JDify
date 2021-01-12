68b1bdfbc428efd0699ec4c3e4acb2fc2ed6013a
wangnmhb <nengmiao.wnm@alibaba-inc.com>
2021-01-12 17:20:56 +0800

    DalaranAutoConfiguration add scan packages

--- core/src/main/java/io/terminus/dalaran/core/spring/DalaranAutoConfiguration.java
+++ core/src/main/java/io/terminus/dalaran/core/spring/DalaranAutoConfiguration.java
@@ -15,9 +15,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "io.terminus.dalaran.core")
 public class DalaranAutoConfiguration {
 
     @Bean
