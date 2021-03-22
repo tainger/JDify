771707b3981d38b532f9d0b7f50f1058c32a7d33
liangchen <cl287152>
2021-03-22 15:31:33 +0800

    添加校验参数

--- console/src/main/java/io/terminus/dalaran/console/service/impl/ClientManagementServiceImpl.java
+++ console/src/main/java/io/terminus/dalaran/console/service/impl/ClientManagementServiceImpl.java
@@ -45,15 +45,15 @@ public class ClientManagementServiceImpl implements ClientManagementService {
     }
 
     @Override
-    public void delete(String appKey) {
-        ClientEntity entity = repository.findByAppKey(appKey);
+    public void delete(String resourceKey) {
+        ClientEntity entity = repository.findByResourceKey(resourceKey);
         entity.setExist(false);
         repository.save(entity);
     }
 
     @Override
-    public ClientDTO detail(String appKey) {
-        ClientEntity entity = repository.findByAppKey(appKey);
+    public ClientDTO detail(String resourceKey) {
+        ClientEntity entity = repository.findByResourceKey(resourceKey);
         if (entity != null) {
             return toDTO(entity);
         }
@@ -85,7 +85,7 @@ public class ClientManagementServiceImpl implements ClientManagementService {
     private ClientDTO toDTO(ClientEntity entity) {
         ClientDTO dto = new ClientDTO();
         try {
-            BeanUtils.copyProperties(entity, dto);
+            BeanUtils.copyProperties(dto, entity);
         }catch (Exception e) {
             e.printStackTrace();
         }
@@ -96,7 +96,7 @@ public class ClientManagementServiceImpl implements ClientManagementService {
     private ClientEntity toEntity(ClientDTO dto) {
         ClientEntity entity = new ClientEntity();
         try {
-            BeanUtils.copyProperties(dto, entity);
+            BeanUtils.copyProperties(entity, dto);
         }catch (Exception e) {
             e.printStackTrace();
         }
