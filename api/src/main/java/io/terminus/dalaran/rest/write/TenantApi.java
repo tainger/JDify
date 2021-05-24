1c1fcb0471044b297a2127a7a7e42af025ab8170
jiazhiyuan <jiazhiyuan.jzy@alibaba-inc.com>
2021-05-24 08:58:20 +0800

    feature: 租户注接口

--- api/src/main/java/io/terminus/dalaran/rest/write/TenantApi.java
+++ api/src/main/java/io/terminus/dalaran/rest/write/TenantApi.java
@@ -2,10 +2,12 @@ package io.terminus.dalaran.rest.write;
 
 
 import io.terminus.dalaran.model.BasicResponse;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "/api/tenant", produces = {"application/json; charset=UTF-8"})
 public interface TenantApi {
 
+    @PostMapping(value = "/register")
     BasicResponse register();
 }
--- console/src/main/java/io/terminus/dalaran/console/rest/TenantManagement.java
+++ console/src/main/java/io/terminus/dalaran/console/rest/TenantManagement.java
@@ -1,16 +1,11 @@
 package io.terminus.dalaran.console.rest;
 
-import io.terminus.dalaran.DalaranConsoleConstants;
 import io.terminus.dalaran.console.service.TenantService;
 import io.terminus.dalaran.core.resource.property.PropertyService;
 import io.terminus.dalaran.model.BasicResponse;
 import io.terminus.dalaran.rest.write.TenantApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
