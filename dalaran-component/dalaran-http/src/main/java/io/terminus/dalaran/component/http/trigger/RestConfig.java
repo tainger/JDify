acc9c7419937c4d6c6bba007f620f24c24fbbe63
liangchen <cl287152>
2021-05-31 11:43:09 +0800

    http触发器类型为post时添加param到body

--- dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/RestConfig.java
+++ dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/RestConfig.java
@@ -67,6 +67,6 @@ public class RestConfig extends AllModelConfig implements AuthenticatorConfig<Da
     @ConfigFieldInfo(label = "超时时间(ms)", inputType = FieldInputType.Integer, defaultValue = "3000")
     private Long timeout = 3000L;
 
-    @ConfigFieldInfo(label = "参数", inputType = FieldInputType.String)
+    @ConfigFieldInfo(label = "参数", inputType = FieldInputType.String, required = false)
     private String params;
 }
--- dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/processor/BodyProcessor.java
+++ dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/processor/BodyProcessor.java
@@ -1,8 +1,12 @@
 package io.terminus.dalaran.component.http.trigger.processor;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Splitter;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
@@ -17,14 +21,22 @@ public class BodyProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        Map<String, String> body = exchange.getIn().getBody(Map.class);
+        if (exchange.getIn().getBody() instanceof JSONArray) {
+            return;
+        }
+        Map<String, Object> body = exchange.getIn().getBody(Map.class);
         Map<String, String> paramMap;
-        String[] paramArray = params.split(",");
-        String queryString = exchange.getIn().getHeader(Exchange.HTTP_QUERY, String.class);
-        if (!StringUtils.isEmpty(queryString)) {
-            paramMap = Splitter.on("&").withKeyValueSeparator("=").split(queryString);
-            for (String param : paramArray) {
-                body.put(param, paramMap.get(param));
+        if (!StringUtils.isBlank(params)) {
+            String[] paramArray = StringUtils.split(StringUtils.replaceChars(params, " ", ""), ",");
+            String queryString = exchange.getIn().getHeader(Exchange.HTTP_QUERY, String.class);
+            if (!StringUtils.isBlank(queryString)) {
+                paramMap = Splitter.on("&").withKeyValueSeparator("=").split(queryString);
+                if (body == null) {
+                    body = new JSONObject();
+                }
+                for (String param : paramArray) {
+                    body.put(param, paramMap.get(param));
+                }
             }
         }
         exchange.getOut().setBody(body);
