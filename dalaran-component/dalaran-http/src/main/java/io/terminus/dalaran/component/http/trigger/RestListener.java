d3bf843703128e7a302ca72cf1ebaa67014e904f
jingdi <jingdi@jingdideMacBook-Pro.local>
2021-07-15 19:34:26 +0800

    save

--- dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/RestListener.java
+++ dalaran-component/dalaran-http/src/main/java/io/terminus/dalaran/component/http/trigger/RestListener.java
@@ -79,8 +79,8 @@ public class RestListener implements DalaranTrigger<RestConfig>, DalaranTriggerA
             }
             route.marshal().json(JsonLibrary.Fastjson);
         } else {
-            route.unmarshal().json(JsonLibrary.Fastjson);
             if (StringUtils.isNotBlank(config.getAuthenticatorId())) {
+                route.unmarshal().json(JsonLibrary.Fastjson);
                 route.process(new AuthenticatorProcessor(authenticator, redisService));
             }
             route.convertBodyTo(String.class);
