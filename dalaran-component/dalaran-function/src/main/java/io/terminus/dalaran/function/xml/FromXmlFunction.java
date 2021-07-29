705902aa8e931c7cdccbec567ad0f50fd7d83a3c
jingdi <jingdi@jingdideMacBook-Pro.local>
2021-07-29 10:42:55 +0800

    fix xml function

--- dalaran-component/dalaran-function/src/main/java/io/terminus/dalaran/function/xml/FromXmlFunction.java
+++ dalaran-component/dalaran-function/src/main/java/io/terminus/dalaran/function/xml/FromXmlFunction.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.terminus.dalaran.core.component.annotation.MappingFunction;
 import org.apache.commons.lang3.StringUtils;
+import org.json.XML;
 
 import java.io.IOException;
 
@@ -16,14 +17,14 @@ public class FromXmlFunction {
     private final ObjectMapper xmlMapper = new XmlMapper();
 
     public Object execute(String data, String type) throws IOException {
-        xmlMapper.registerModule(new SimpleModule().addDeserializer(
-                JsonNode.class,
-                new DuplicateToArrayJsonNodeDeserializer()
-        ));
-        JsonNode node = xmlMapper.readTree(format(data));
-        ObjectMapper jsonMapper = new ObjectMapper();
-        String json = jsonMapper.writeValueAsString(node);
-        return JSON.parse(json);
+//        xmlMapper.registerModule(new SimpleModule().addDeserializer(
+//                JsonNode.class,
+//                new DuplicateToArrayJsonNodeDeserializer()
+//        ));
+//        JsonNode node = xmlMapper.readTree(format(data));
+//        ObjectMapper jsonMapper = new ObjectMapper();
+//        String json = jsonMapper.writeValueAsString(node);
+        return JSON.parse(XML.toJSONObject(format(data)).toString());
     }
 
     private String format(String data) {
